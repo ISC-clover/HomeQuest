f3fe5dc55adc4bff41cdb9ea1dfbedc004a56c9e
9syk <chlorine17cl.udn@gmail.com>
2026-01-20 13:21:28 +0900

    dbセットアップ

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from database import Base, engine
+import models
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
     return {"message": "HomeQuest backend is running!"}
+
+@app.get("/health")
+def health():
+    return {"status":"ok"}
+
+@app.post("/users")
+def create_user(name: str):
+    return {"user_id":1, "name":name}
+
+@app.post("/groups")
+def create_group(name: str):
+    return {"group_id":1, "name":name}
