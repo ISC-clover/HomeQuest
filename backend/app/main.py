dea8166e5ec006cbf192a0c67307baa9408b3a5a
9syk <chlorine17cl.udn@gmail.com>
2026-01-20 13:52:18 +0900

    ユーザー登録

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,23 +1,16 @@
-from fastapi import FastAPI
-from database import Base, engine
-import models
+from fastapi import FastAPI, Depends
+from database import Base, engine, get_db
+import models, schemas, crud
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
-@app.get("/")
-def root():
-    return {"message": "HomeQuest backend is running!"}
+@app.post("/users", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    return crud.create_user(db, user)
 
-@app.get("/health")
-def health():
-    return {"status":"ok"}
-
-@app.post("/users")
-def create_user(name: str):
-    return {"user_id":1, "name":name}
-
-@app.post("/groups")
-def create_group(name: str):
-    return {"group_id":1, "name":name}
+@app.get("/users", response_model=list[schemas.User])
+def read_users(db: Session = Depends(get_db)):
+    return crud.get_users(db)
