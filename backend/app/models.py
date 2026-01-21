8a2d0c04b846457741cfa29838a454d71079a633
9syk <chlorine17cl.udn@gmail.com>
2026-01-21 10:52:04 +0900

    グループの詳細設計

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from database import Base
 
 class User(Base):
@@ -12,10 +12,14 @@ class Group(Base):
     
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
+    owner_user_id = Column(Integer, ForeignKey("users.id"))
+    reward_shop = Column(String, nullable=True)
 
 class UserGroup(Base):
     __tablename__ = "user_groups"
     
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id"))
-    group_id = Column(Integer, ForeignKey("groups.id"))    
+    group_id = Column(Integer, ForeignKey("groups.id"))  
+    points = Column(Integer, default=0)
+    is_host = Column(Boolean, default=False)
